ebfc78cdea87ae1b63eb35c72a49b7e26edf8ba8
root <leavesbnw@163.com>
2015-12-21 14:26:30 +0800

    tornado session with redis

---
+++ session.py
@@ -0,0 +1,65 @@
+#/usr/bin/python
+# coding: utf-8
+
+import uuid
+import hmac
+import ujson
+import hashlib
+import redis
+
+class Session(dict):
+	def __init__(self, session_manager, request_handler):
+
+		self.session_manager = session_manager
+		self.request_handler = request_handler
+		self.session_id = session_manager.getid(request_handler)
+	
+	def save(self):
+		self.session_manager.set(self.request_handler, self)
+
+
+class SessionManager(object):
+	def __init__(self, secret, store_options, session_timeout):
+		self.secret = secret
+		self.session_timeout = session_timeout
+		try:
+			if store_options['redis_pass']:
+				self.redis = redis.StrictRedis(host=store_options['redis_host'], port=store_options['redis_port'], password=store_options['redis_pass'])
+			else:
+				self.redis = redis.StrictRedis(host=store_options['redis_host'], port=store_options['redis_port'])
+		except Exception as e:
+			print e 
+			
+
+	def getid(self,request_handler = None):
+		if (request_handler == None):
+			session_id = None
+		else:
+			session_id = request_handler.get_secure_cookie("session_id")
+
+		if session_id == None:
+			session_exists = False
+			session_id = self._generate_id()
+		else:
+			session_exists = True
+
+		if session_exists and not self.redis.exists(session_id):
+			session_id = self._generate_id()
+		return session_id
+	
+	def set(self, request_handler, session):
+		request_handler.set_secure_cookie("session_id", session.session_id)
+
+		session_data = ujson.dumps(dict(session.items()))
+
+		self.redis.setex(session.session_id, self.session_timeout, session_data)
+
+
+	def _generate_id(self):
+		new_id = hashlib.sha256(self.secret + str(uuid.uuid4()))
+		return new_id.hexdigest()
+
+
+class InvalidSessionException(Exception):
+	pass
+
